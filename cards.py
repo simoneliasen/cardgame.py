c20ad40dcffc12e14b00ad384064ee5daf15c809
Simon Eliasen <simoneliasen@gmail.com>
2019-07-29 22:12:52 +0200

    initialized repository w. base

---
+++ cards.py
@@ -0,0 +1,66 @@
+import random
+
+class Card:
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
+
+    def drawCard(self):
+        return self.cards.pop()
+
+
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for v in range(1, 14):
+                self.cards.append(Card(s, v))
+
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+    def drawCard(self):
+        return self.cards.pop()
+
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self. hand = []
+
+    def draw(self, deck):
+        self.hand.append(deck.drawCard())
+        return self
+
+    def showHand(self):
+        for card in self.hand:
+            card.show()
+
+
+#Create deck, and shuffle
+deck = Deck()
+deck.shuffle()
+
+#Draw card and show
+card = deck.drawCard()
+card.show()
+
+#Create player instance
+bob = Player("Bob")
+bob.draw(deck)
+bob.showHand()
