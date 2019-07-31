1a63590db12f299e189371039c8bf11692d33249
Mona Lisa <simoneliasen@gmail.comgit>
2019-07-31 23:24:40 +0200

    Game add suits + amount of players and add them to dictionary

--- cards.py
+++ cards.py
@@ -12,8 +12,6 @@ class Card:
         return self.cards.pop()
 
 
-
-
 class Deck:
     def __init__(self):
         self.cards = []
@@ -38,29 +36,68 @@ class Deck:
 
 
 
+class PlayerGroup:
+    def __init__(self):
+        self.suits= []
+
+    def player_dict(self):
+        player_dict = {}
+        keys = range(int(input("How many players? ")))
+        values = self.suits
+        for i in keys:
+            self.suits.append(input("Pick a suit: "))
+            player_dict[i] = values[i]
+        print(player_dict)
+
+class PointSystem:
+    pass
+
+
+
 class Player:
-    def __init__(self, name):
-        self.name = name
-        self. hand = []
+    def __init__(self, points, suit):
+        self.suit = suit
+        self.points = points
 
-    def draw(self, deck):
-        self.hand.append(deck.drawCard())
-        return self
 
-    def showHand(self):
-        for card in self.hand:
-            card.show()
-
+# Instantiate PlayerGroup + print player_dict
+players = PlayerGroup()
+players.player_dict()
 
 #Create deck, and shuffle
 deck = Deck()
 deck.shuffle()
 
-#Draw card and show
+#Draws a card and show it
 card = deck.drawCard()
 card.show()
 
-#Create player instance
-bob = Player("Bob")
-bob.draw(deck)
-bob.showHand()
+# If card is equal to player_suit: +1 point
+
+#Draw a new card
+
+#if all players has above 1 point, draw minus card
+
+#if suit == minus card: minus 1 point
+
+#Make minus card only able to run once, every "milestone"
+
+
+#When one player gets 5 points, the player wins
+
+
+
+        # for i in range (int(input("How many players? "))):
+        #     self.suit = input("Pick a suit: ")
+    #
+    # def amount(self):
+    #     for i in range(int(input("How many players? "))):
+    #         self.player_number.append(i)
+
+    # def playersuits(self):
+    #     for i in self.player_number:
+    #         self.suit = input("Pick a suit: ")
+
+    # def printplayers(self):
+    #     for i in self.player_number:
+    #         print(i)
