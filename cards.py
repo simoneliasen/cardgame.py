4985a9559cf027f9c93e2fab5a1b571e368fe0ed
Mona Lisa <simoneliasen@gmail.comgit>
2019-08-08 00:10:30 +0200

    subractbonus intialize ready

--- cards.py
+++ cards.py
@@ -1,6 +1,6 @@
 import random
 
-#Card
+#CARD
 class Card:
     def __init__(self, cardsuit, val):
         self.cardsuit = cardsuit
@@ -9,7 +9,7 @@ class Card:
     def show(self):
         print("{} of {}".format(self.value, self.cardsuit))
 
-#Deck of cards
+#DECK OF CARDS
 class Deck:
     def __init__(self):
         self.cards = []
@@ -32,7 +32,7 @@ class Deck:
     def drawCard(self):
         return self.cards.pop()
 
-#Player Management
+#PLAYER MANAGEMENT
 class PlayerGroup:
     def __init__(self):
         self.amount= []
@@ -55,19 +55,51 @@ class PlayerGroup:
         for x in self.player_list:
             if x.playersuit == card.cardsuit:
                 x.points += 1
-# BETA NOT DONE -- add extra card drawn
-#    def subract_bonus(self):        
-#        if all(self.points > 1 for x in self.player_list):
-#            for x in self.player_list:
-#                if x.playersuit == card.cardsuit:
-#                    x.points -= 1
+
+# BETA NOT DONE 
+    def subtract_bonus1(self):
+        if all(player.points >= 1 for player in self.player_list): #Works until here
+                print('Cardbonus1')
+
+    def subtract_bonus2(self):
+        if all(player.points >= 2 for player in self.player_list): #Works until here
+                print('Cardbonus2')
+
+    def subtract_bonus3(self):
+        if all(player.points >= 3 for player in self.player_list): #Works until here
+                print('Cardbonus3')
+
+    def subtract_bonus4(self):
+        if all(player.points >= 4 for player in self.player_list): #Works until here
+                print('Cardbonus4')
+
+#EXAMPLE:
+#Draw card if all >= 1
+#If cardsuit = playersuits =- 1
+#Don't run again if it returns true
+
+#RANDOM SPAGHETTI BELOW
+        # run_once = 0
+        # while run_once == 0:
+            # if all(player.points >= 1 for player in self.player_list): #Works until here
+            #     print('Hello world')
+                # run_once += 1
+            # else:
+            #     break
+
+
+                # for x in self.player_list:
+                #     if x.playersuit == card.cardsuit:
+                #         x.points -= 1
+                #         run_once += 1
+                #         print('It worked')
 
     def winner(self):
         for x in self.player_list:
             if x.points == 5:
                 return f'Player{x.name} won!'
 
-# Player Object (instatiated in PlayerGroup)
+# PLAYBER OBJECT (instantiated via PlayerGroup)
 class Player:
     def __init__(self, name, playersuit, points):
         self.name = name
@@ -81,7 +113,6 @@ class Player:
 PlayerManage = PlayerGroup()
 PlayerManage.amount_of_players()
 PlayerManage.create_players()
-
 #Create deck, and shuffle
 deck = Deck()
 deck.shuffle()
@@ -90,13 +121,7 @@ deck.shuffle()
 card = deck.drawCard()
 card.show()
 
-#Add points
-PlayerManage.add_points()
-#Print player objects
-PlayerManage.print_players()
-#Draws a card and show it
-card = deck.drawCard()
-card.show()
+## A BOUNCH OF SPAGHETTI DOWNWARDS
 
 #Add points
 PlayerManage.add_points()
@@ -105,6 +130,8 @@ PlayerManage.print_players()
 #Draws a card and show it
 card = deck.drawCard()
 card.show()
+#Subtract bonus check
+PlayerManage.subtract_bonus1()
 
 #Add points
 PlayerManage.add_points()
@@ -113,6 +140,8 @@ PlayerManage.print_players()
 #Draws a card and show it
 card = deck.drawCard()
 card.show()
+#Subtract bonus check
+PlayerManage.subtract_bonus1()
 
 #Add points
 PlayerManage.add_points()
@@ -121,6 +150,8 @@ PlayerManage.print_players()
 #Draws a card and show it
 card = deck.drawCard()
 card.show()
+#Subtract bonus check
+PlayerManage.subtract_bonus1()
 
 #Add points
 PlayerManage.add_points()
@@ -129,6 +160,8 @@ PlayerManage.print_players()
 #Draws a card and show it
 card = deck.drawCard()
 card.show()
+#Subtract bonus check
+PlayerManage.subtract_bonus1()
 
 #Add points
 PlayerManage.add_points()
@@ -137,6 +170,8 @@ PlayerManage.print_players()
 #Draws a card and show it
 card = deck.drawCard()
 card.show()
+#Subtract bonus check
+PlayerManage.subtract_bonus1()
 
 #Add points
 PlayerManage.add_points()
@@ -145,52 +180,9 @@ PlayerManage.print_players()
 #Draws a card and show it
 card = deck.drawCard()
 card.show()
+#Subtract bonus check
+PlayerManage.subtract_bonus1()
 
-#Add points
-PlayerManage.add_points()
-#Print player objects
-PlayerManage.print_players()
-#Draws a card and show it
-card = deck.drawCard()
-card.show()
-
-#Add points
-PlayerManage.add_points()
-#Print player objects
-PlayerManage.print_players()
-#Draws a card and show it
-card = deck.drawCard()
-card.show()
-
-#Add points
-PlayerManage.add_points()
-#Print player objects
-PlayerManage.print_players()
-#Draws a card and show it
-card = deck.drawCard()
-card.show()
-
-#Add points
-PlayerManage.add_points()
-#Print player objects
-PlayerManage.print_players()
-#Draws a card and show it
-card = deck.drawCard()
-card.show()
-
-#Add points
-PlayerManage.add_points()
-#Print player objects
-PlayerManage.print_players()
-#Draws a card and show it
-card = deck.drawCard()
-card.show()
-
+#Print winner
 print(PlayerManage.winner())
-
-#Functions to add:
-#1. Pulle extra card if all points >= 1,2,3,4:
-# Print only once (while loop?)
-#2. Display message with points at last
-#3. Integrate with flask
 
